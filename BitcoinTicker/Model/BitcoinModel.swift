bfc56a18b83d3f0f5bd84ed458bb48776d241302
Unknown <t19960804@gmail.com>
2018-07-25 12:12:46 +0800

    0725
    最高/最低/平均

---
+++ BitcoinTicker/Model/BitcoinModel.swift
@@ -0,0 +1,15 @@
+//
+//  BitcoinModel.swift
+//  BitcoinTicker
+//
+//  Created by t19960804 on 2018/7/24.
+//  Copyright © 2018年 London App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Bitcoin {
+    var highPrice: Double = 0.0
+    var lowPrice: Double = 0.0
+    var averagePrice: Double = 0.0
+    var coinSign: String = ""
+}
--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -7,30 +7,56 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import SwiftyJSON
+import Alamofire
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySignArray = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
     var finalURL = ""
-
+    var bitcoinModel = Bitcoin()
+    let globalQueue = DispatchQueue.global()
+    let mainQueue  = DispatchQueue.main
     //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var highPriceLabel: UILabel!
+    @IBOutlet weak var lowPriceLabel: UILabel!
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
        
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
     
+    //How many columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
     
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    //當pickerView被轉動時
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        globalQueue.async {
+            let finalURL = self.baseURL + self.currencyArray[row]
+            self.getBitcoinData(url: "\(finalURL)",currencySign: self.currencySignArray[row])
+        }
+        
+        
+    }
     
     
     
@@ -38,24 +64,25 @@ class ViewController: UIViewController {
 //    //MARK: - Networking
 //    /***************************************************************/
 //    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
+    func getBitcoinData(url: String , currencySign: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Sucess! Got the Bitcoin data")
+                    let bitcoinJSON : JSON = JSON(response.result.value!)
+                    
+                    
+                    self.updateBitcoinData(json: bitcoinJSON , theSign: currencySign)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
 //
 //    
 //    
@@ -64,19 +91,27 @@ class ViewController: UIViewController {
 //    //MARK: - JSON Parsing
 //    /***************************************************************/
 //    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    func updateBitcoinData(json : JSON , theSign: String) {
+        let bitcoinAveragePrice = json["averages"]["week"].doubleValue
+        let bitcoinHighPrice = json["high"].doubleValue
+        let bitcoinLowPrice = json["low"].doubleValue
+        let bitcoinSign = theSign
+        bitcoinModel.averagePrice = bitcoinAveragePrice
+        bitcoinModel.highPrice = bitcoinHighPrice
+        bitcoinModel.lowPrice = bitcoinLowPrice
+        bitcoinModel.coinSign = bitcoinSign
+
+        mainQueue.async {
+            self.updateBitcoinUI()
+        }
+        
+    }
+    func updateBitcoinUI()
+    {
+        bitcoinPriceLabel.text = "\(bitcoinModel.coinSign) \(bitcoinModel.averagePrice)"
+        highPriceLabel.text = "\(bitcoinModel.coinSign) \(bitcoinModel.highPrice)"
+        lowPriceLabel.text = "\(bitcoinModel.coinSign) \(bitcoinModel.lowPrice)"
+    }
 
 
 
